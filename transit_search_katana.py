a556dfbe3c5849cd12bf15340ace0fac2d5f33d1
Ryan Morris <z5318114@katana1.gen>
2021-11-09 16:42:13 +1100

    updated transit_search_katana with output table, rerun bool, and default parameters

--- transit_search_katana.py
+++ transit_search_katana.py
@@ -23,28 +23,53 @@ from transitleastsquares.stats import calculate_stretch
 from transitleastsquares.stats import all_transit_times
 from transitleastsquares.transit import fractional_transit
 import transitleastsquares.tls_constants as tls_constants
+from eleanor import mast
 
 
 
 class transit:
     """
     parameters:
-        TIC: TIC ID (112099249)
+        ra: right acension of the target star
+        dec: declination of the target star
         directory: directory where folders of light curves are stored, separated by source (eleanor vs lightkurve) (/data/wallaby/rmorris/GALAH/)
+        rerun: if True, run every target given, if False, only run a target if it has not been run before
         
     """
     
-    def __init__(self,TIC, directory):
-        self.TIC = TIC
+    def __init__(self, ra, dec, directory, rerun=True):
+        TIC_info = mast.tic_from_coords((float(ra), float(dec)))
+        self.ra = float(ra)
+        self.dec = float(dec)
+        self.TIC = TIC_info[0]
         self.dir = directory
         
-        #try:
-        self.file_finder()
-        self.data_read_in()
-        self.period_finder()
-        self.plot()
-        #except:
-        #    print('{} failed'.format(self.TIC))
+        ### setting defaults for writeout in case the self.plot() fails in the try loop
+        self.SDE = -1
+        self.FAP = -1
+        self.distinct_transit_count = -1
+        self.empty_transit_count = -1
+        self.odd_even_mismatch = -1
+
+        if rerun == False:
+            self.check_done()
+            if self.run == 1:
+                #try:
+                self.file_finder()
+                self.data_read_in()
+                #except exception as e:
+                #    print('{} failed'.format(self.TIC))
+                #    print(e)
+                self.writeout()
+            else:
+                print('This target was already run and will not be rerun.')
+        else:
+            #try:
+            self.file_finder()
+            self.data_read_in()
+            #except:
+            #    print('{} failed'.format(self.TIC))
+            self.writeout()
         
     def file_finder(self):
         os.chdir("{}".format(self.dir))
@@ -64,95 +89,103 @@ class transit:
         self.sources = sources
 
     def data_read_in(self):
-        for index, files in enumerate(self.filepaths):
-            print(index, files)
-            if self.sources[index] == self.ffi_dir:
-                ### this is 30/10 minute cadence data
-                print('doing 30-min data')
-                data = fits.open('{}'.format(files))
-                cx = data[1].data['X_CENTROID']
-                cy = data[1].data['Y_CENTROID']
-                sector = data[0].header['SECTOR']
-                camera = data[0].header['CAMERA']
-                ccd = data[0].header['CCD']
-                time = data[1].data['TIME']
-                raw_flux = data[1].data['RAW_FLUX']
-                flux_err = data[1].data['FLUX_ERR']
-                quality_flags = data[1].data['QUALITY']
-                bkg_flux = data[1].data['FLUX_BKG']
-                tpfs = data[1].data['TPF']
-                biggest_ap = data[2].data['4_circle_exact']
+        if self.filepaths:
+            for index, files in enumerate(self.filepaths):
+                print(index, files)
+                if self.sources[index] == self.ffi_dir:
+                    ### this is 30/10 minute cadence data
+                    print('doing 30-min data')
+                    data = fits.open('{}'.format(files))
+                    cx = data[1].data['X_CENTROID']
+                    cy = data[1].data['Y_CENTROID']
+                    sector = data[0].header['SECTOR']
+                    camera = data[0].header['CAMERA']
+                    ccd = data[0].header['CCD']
+                    time = data[1].data['TIME']
+                    raw_flux = data[1].data['RAW_FLUX']
+                    flux_err = data[1].data['FLUX_ERR']
+                    quality_flags = data[1].data['QUALITY']
+                    bkg_flux = data[1].data['FLUX_BKG']
+                    tpfs = data[1].data['TPF']
+                    biggest_ap = data[2].data['4_circle_exact']
 
-                ### this only applies to eleanor light curves at the moment
-                ### will have to adjust later for 2-min data or move this into eleanor only section
-                q = quality_flags == 0
-                med_image = np.nanmedian(tpfs, axis=0)
-                self.med_image = med_image
-                mask_dict = {}
-                for ind, row in enumerate(med_image):
-                    for ind2, col in enumerate(row):
-                        if biggest_ap[ind][ind2] == 0:
-                            value = med_image[ind][ind2]
-                            mask_dict[value] = (ind, ind2)
-                mask_dict = dict(sorted(mask_dict.items(), key=lambda item: item[0]))
-                lowest = list(mask_dict)[0:4]
-                first = np.zeros(4, dtype=int)
-                second = np.zeros(4, dtype=int)
-                for ilow, index3 in enumerate(lowest):
-                    first[ilow] = mask_dict[index3][0]
-                    second[ilow] = mask_dict[index3][1]
-                regressors = np.array([tpfs[:,0,0], tpfs[:,0,-1], tpfs[:,-1,-1], tpfs[:,-1,0], tpfs[:,first[0],second[0]], tpfs[:,first[1],second[1]], tpfs[:,first[2],second[2]], tpfs[:,first[3],second[3]]]).T
-                corr_flux = corrected_flux(data[1], time=time, flux=raw_flux, quality=quality_flags,centroid_xs=cx, centroid_ys=cy, bkg=bkg_flux, regressors=regressors, sector=sector, camera=camera, chip=ccd)
-                norm_flux = corr_flux[q]/np.nanmedian(corr_flux[q])
+                    ### this only applies to eleanor light curves at the moment
+                    ### will have to adjust later for 2-min data or move this into eleanor only section
+                    q = quality_flags == 0
+                    med_image = np.nanmedian(tpfs, axis=0)
+                    self.med_image = med_image
+                    mask_dict = {}
+                    for ind, row in enumerate(med_image):
+                        for ind2, col in enumerate(row):
+                            if biggest_ap[ind][ind2] == 0:
+                                value = med_image[ind][ind2]
+                                mask_dict[value] = (ind, ind2)
+                    mask_dict = dict(sorted(mask_dict.items(), key=lambda item: item[0]))
+                    lowest = list(mask_dict)[0:4]
+                    first = np.zeros(4, dtype=int)
+                    second = np.zeros(4, dtype=int)
+                    for ilow, index3 in enumerate(lowest):
+                        first[ilow] = mask_dict[index3][0]
+                        second[ilow] = mask_dict[index3][1]
+                    regressors = np.array([tpfs[:,0,0], tpfs[:,0,-1], tpfs[:,-1,-1], tpfs[:,-1,0], tpfs[:,first[0],second[0]], tpfs[:,first[1],second[1]], tpfs[:,first[2],second[2]], tpfs[:,first[3],second[3]]]).T
+                    corr_flux = corrected_flux(data[1], time=time, flux=raw_flux, quality=quality_flags,centroid_xs=cx, centroid_ys=cy, bkg=bkg_flux, regressors=regressors, sector=sector, camera=camera, chip=ccd)
+                    norm_flux = corr_flux[q]/np.nanmedian(corr_flux[q])
 
-                rolling_avg = np.zeros(len(bkg_flux[q]))
-                for ind, val in enumerate(bkg_flux[q][11:-11]):
-                    rolling_avg[ind] = np.mean(bkg_flux[q][ind-11:ind+11])
-                grad = np.gradient(rolling_avg)
-                vals = np.full(shape=np.shape(grad), fill_value=3)
-                use_bool = np.less(abs(grad),vals)
+                    rolling_avg = np.zeros(len(bkg_flux[q]))
+                    for ind, val in enumerate(bkg_flux[q][11:-11]):
+                        rolling_avg[ind] = np.mean(bkg_flux[q][ind-11:ind+11])
+                    grad = np.gradient(rolling_avg)
+                    vals = np.full(shape=np.shape(grad), fill_value=3)
+                    use_bool = np.less(abs(grad),vals)
 
-                lctime = time[q][use_bool]
-                lcflux = norm_flux[use_bool]
-                lcferr = flux_err[q][use_bool]
-            
-            elif self.sources[index] == self.fast_dir:
-                ### this is 2 minute/20 second data
-                ### not sure whether to use SAP or PDC because tpfs?
-                ### could just skip and set corr flux to PDC later
-                data = fits.open('{}'.format(files))
-                print('doing 2-min data')
-                time = data[1].data['TIME']
-                raw_flux = data[1].data['PDCSAP_FLUX']
-                flux_err = data[1].data['PDCSAP_FLUX_ERR']
-                quality_flags = data[1].data['QUALITY']
+                    lctime = time[q][use_bool]
+                    lcflux = norm_flux[use_bool]
+                    lcferr = flux_err[q][use_bool]
+                
+                elif self.sources[index] == self.fast_dir:
+                    ### this is 2 minute/20 second data
+                    ### not sure whether to use SAP or PDC because tpfs?
+                    ### could just skip and set corr flux to PDC later
+                    data = fits.open('{}'.format(files))
+                    print('doing 2-min data')
+                    time = data[1].data['TIME']
+                    raw_flux = data[1].data['PDCSAP_FLUX']
+                    flux_err = data[1].data['PDCSAP_FLUX_ERR']
+                    quality_flags = data[1].data['QUALITY']
 
-                q = quality_flags == 0
-                normflux = raw_flux[q]/np.nanmedian(raw_flux[q])
+                    q = quality_flags == 0
+                    normflux = raw_flux[q]/np.nanmedian(raw_flux[q])
 
-                lctime = time[q]
-                lcflux = normflux
-                lcferr = flux_err[q]
-            
-            
+                    lctime = time[q]
+                    lcflux = normflux
+                    lcferr = flux_err[q]
+                
+                
 
-            if index == 0:
-                lc = lk.LightCurve(time=lctime, flux=lcflux, flux_err=lcferr).remove_outliers(sigma_lower=7, sigma_upper=3).flatten(window_length=51,break_tolerance=15)
-            else:
-                new_lc = lk.LightCurve(time=lctime, flux=lcflux, flux_err=lcferr).remove_outliers(sigma_lower=7, sigma_upper=3).flatten(window_length=51,break_tolerance=15)
-                lc = lc.append(new_lc)
-        ### after all sectors done, put the whole lc object in self?
-        print('before')
-        self.lc = lc
-        print('after')
+                if index == 0:
+                    lc = lk.LightCurve(time=lctime, flux=lcflux, flux_err=lcferr).remove_outliers(sigma_lower=7, sigma_upper=3).flatten(window_length=51,break_tolerance=15)
+                else:
+                    new_lc = lk.LightCurve(time=lctime, flux=lcflux, flux_err=lcferr).remove_outliers(sigma_lower=7, sigma_upper=3).flatten(window_length=51,break_tolerance=15)
+                    lc = lc.append(new_lc)
+            ### after all sectors done, put the whole lc object in self?
+            self.lc = lc
+            self.period_finder()
+            self.plot()
 
     def period_finder(self):
+        print('Searching for transit...')
         ### performing TLS fit of data, carry over some to plotting I guess
         periodogram = transitleastsquares(self.lc.time.value, self.lc.flux.value, self.lc.flux_err.value)
         results = periodogram.power(period_max=75.0, n_transits_min=1, oversampling_factor=5)
         self.best_period = results.period
         self.best_p_unc = results.period_uncertainty
         self.results = results
+
+        self.SDE = results.SDE
+        self.FAP = results.FAP
+        self.distinct_transit_count = results.distinct_transit_count
+        self.empty_transit_count = results.empty_transit_count
+        self.odd_even_mismatch = results.odd_even_mismatch
 
         maxwidth_in_samples = int(np.max(periodogram.durations) * np.size(periodogram.y))
         transit_times = all_transit_times(results.T0, periodogram.t, results.period)
@@ -186,16 +219,13 @@ class transit:
                 u=periodogram.u,
                 limb_dark=periodogram.limb_dark,
             )
-        #print(np.zeros(len(self.results.even_model)))
-        #print(len(self.results.even_model))
-        #print(self.results.even_model)
         step = (self.results.model_folded_phase[-1]-self.results.model_folded_phase[0])/len(self.results.even_model)
         self.times = np.arange(self.results.model_folded_phase[0], self.results.model_folded_phase[-1], step)
 
 
     def plot(self):
+        print('Beginning plotting...')
         ### plotting something like DV report
-        #intransit_bool = transit_mask(self.lc.time.value,self.results.period,self.results.duration,self.results.T0)
         ### below from ben's code
         lkf = lk.LightCurve(self.results.folded_phase, self.results.folded_y).bin(0.01)
 
@@ -236,7 +266,7 @@ class transit:
         for n in range(2, 10):
             ax.axvline(n*self.results.period, alpha=0.4, lw=1, linestyle="dashed")
             ax.axvline(self.results.period / n, alpha=0.4, lw=1, linestyle="dashed")
-        plt.ylabel(r'SDE')
+        plt.ylabel('Power')
         plt.xlabel('Period (days)')
         plt.plot(self.results.periods, self.results.power, color='black', lw=0.5)
         plt.xlim(0, max(self.results.periods));
@@ -316,11 +346,25 @@ class transit:
 
         f4 = fig.add_subplot(spec[5:6,0:])
         plt.plot(lkf.time.value, lkf.flux.value, 'k')
-        #plt.xlim(-1, 1650)
-        #print(lkf.time.value)
-        #print(lkf.flux.value)
         print(self.results.depth_mean_even, self.results.depth_mean_odd)
 
         plt.savefig(self.dir + 'plots/' + str(self.TIC) + '.png')
 
-        
+    def writeout(self):
+        print('Writing out results...')
+        totalfile = open('/home/z5318114/targets_plotted.txt',"a")
+        totalfile.write(str(self.TIC)+'\t'+str(self.ra)+'\t'+str(self.dec)+'\t'+str(self.SDE)+'\t'+str(self.FAP)+'\t'+str(self.distinct_transit_count)+'\t'+str(self.empty_transit_count)+'\t'+str(self.odd_even_mismatch)+'\n')
+        totalfile.close()    
+
+    def check_done(self):
+        print('Checking if target has been run previously...')
+        done_id = []
+        with open('/home/z5318114/targets_plotted.txt','r') as file:
+            reader = csv.reader(file, delimiter='\t')
+            for row in reader:
+                done_id.append(row[0])
+
+        if self.TIC in done_id:
+            self.run = 0
+        else:
+            self.run = 1
